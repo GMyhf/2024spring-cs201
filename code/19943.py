af4de7bfa08915fa05b65a910cf59fdbdf85fdd7
GMyhf <57581772+GMyhf@users.noreply.github.com>
2024-04-08 19:07:39 +0800

    Add files via upload

---
+++ code/19943.py
@@ -0,0 +1,86 @@
+class Vertex:  # 19943: 图的拉普拉斯矩阵**, http://cs101.openjudge.cn/practice/19943/
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+
+    def addNeighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
+
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self, nbr):
+        return self.connectedTo[nbr]
+
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self, key):
+        self.numVertices = self.numVertices + 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+
+    def getVertex(self, n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+
+    def __contains__(self, n):
+        return n in self.vertList
+
+    def addEdge(self, f, t, weight=0):
+        if f not in self.vertList:
+            nv = self.addVertex(f)
+        if t not in self.vertList:
+            nv = self.addVertex(t)
+        self.vertList[f].addNeighbor(self.vertList[t], weight)
+
+    def getVertices(self):
+        return self.vertList.keys()
+
+    def __iter__(self):
+        return iter(self.vertList.values())
+
+
+def constructLaplacianMatrix(n, edges):
+    graph = Graph()
+    for i in range(n):  # 添加顶点
+        graph.addVertex(i)
+
+    for edge in edges:  # 添加边
+        a, b = edge
+        graph.addEdge(a, b)
+        graph.addEdge(b, a)
+
+    laplacianMatrix = []  # 构建拉普拉斯矩阵
+    for vertex in graph:
+        row = [0] * n
+        row[vertex.getId()] = len(vertex.getConnections())
+        for neighbor in vertex.getConnections():
+            row[neighbor.getId()] = -1
+        laplacianMatrix.append(row)
+
+    return laplacianMatrix
+
+
+n, m = map(int, input().split())  # 解析输入
+edges = []
+for i in range(m):
+    a, b = map(int, input().split())
+    edges.append((a, b))
+
+laplacianMatrix = constructLaplacianMatrix(n, edges)  # 构建拉普拉斯矩阵
+
+for row in laplacianMatrix:  # 输出结果
+    print(' '.join(map(str, row)))
