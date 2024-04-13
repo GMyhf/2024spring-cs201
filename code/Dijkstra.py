fe3bc506462b790d7c533da4981d056cc0e862fa
Hongfei Yan <yanhf@pku.edu.cn>
2024-04-14 01:14:18 +0800

    initial

---
+++ code/Dijkstra.py
@@ -0,0 +1,106 @@
+import heapq
+import sys
+
+class Vertex:
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+        self.distance = sys.maxsize
+        self.pred = None
+
+    def addNeighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getWeight(self, nbr):
+        return self.connectedTo[nbr]
+
+    def __lt__(self, other):
+        return self.distance < other.distance
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self, key):
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        self.numVertices += 1
+        return newVertex
+
+    def getVertex(self, n):
+        return self.vertList.get(n)
+
+    def addEdge(self, f, t, cost=0):
+        if f not in self.vertList:
+            self.addVertex(f)
+        if t not in self.vertList:
+            self.addVertex(t)
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
+
+def dijkstra(graph, start):
+    pq = []
+    start.distance = 0
+    heapq.heappush(pq, (0, start))
+
+    while pq:
+        currentDist, currentVert = heapq.heappop(pq)
+        for nextVert in currentVert.getConnections():
+            newDist = currentDist + currentVert.getWeight(nextVert)
+            if newDist < nextVert.distance:
+                nextVert.distance = newDist
+                nextVert.pred = currentVert
+                heapq.heappush(pq, (newDist, nextVert))
+
+# 创建图和边
+g = Graph()
+g.addEdge('A', 'B', 4)
+g.addEdge('A', 'C', 2)
+g.addEdge('C', 'B', 1)
+g.addEdge('B', 'D', 2)
+g.addEdge('C', 'D', 5)
+g.addEdge('D', 'E', 3)
+g.addEdge('E', 'F', 1)
+g.addEdge('D', 'F', 6)
+
+# 执行 Dijkstra 算法
+print("Shortest Path Tree:")
+dijkstra(g, g.getVertex('A'))
+
+# 输出最短路径树的顶点及其距离
+for vertex in g.vertList.values():
+    print(f"Vertex: {vertex.id}, Distance: {vertex.distance}")
+
+# 输出最短路径到每个顶点
+def printPath(vert):
+    if vert.pred:
+        printPath(vert.pred)
+        print(" -> ", end="")
+    print(vert.id, end="")
+
+print("\nPaths from Start Vertex 'A':")
+for vertex in g.vertList.values():
+    print(f"Path to {vertex.id}: ", end="")
+    printPath(vertex)
+    print(", Distance: ", vertex.distance)
+
+"""
+Shortest Path Tree:
+Vertex: A, Distance: 0
+Vertex: B, Distance: 3
+Vertex: C, Distance: 2
+Vertex: D, Distance: 5
+Vertex: E, Distance: 8
+Vertex: F, Distance: 9
+
+Paths from Start Vertex 'A':
+Path to A: A, Distance:  0
+Path to B: A -> C -> B, Distance:  3
+Path to C: A -> C, Distance:  2
+Path to D: A -> C -> B -> D, Distance:  5
+Path to E: A -> C -> B -> D -> E, Distance:  8
+Path to F: A -> C -> B -> D -> E -> F, Distance:  9
+"""
