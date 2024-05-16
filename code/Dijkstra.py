69063698358cffabb2faac6ca155fbd22456a05c
Hongfei Yan <yanhf@pku.edu.cn>
2024-05-16 23:10:57 +0800

    initial

---
+++ code/Dijkstra.py
@@ -0,0 +1,33 @@
+# 03424: Candies
+# http://cs101.openjudge.cn/practice/03424/
+import heapq
+
+def dijkstra(N, G, start):
+    INF = float('inf')
+    dist = [INF] * (N + 1)  # 存储源点到各个节点的最短距离
+    dist[start] = 0  # 源点到自身的距离为0
+    pq = [(0, start)]  # 使用优先队列，存储节点的最短距离
+    while pq:
+        d, node = heapq.heappop(pq)  # 弹出当前最短距离的节点
+        if d > dist[node]:  # 如果该节点已经被更新过了，则跳过
+            continue
+        for neighbor, weight in G[node]:  # 遍历当前节点的所有邻居节点
+            new_dist = dist[node] + weight  # 计算经当前节点到达邻居节点的距离
+            if new_dist < dist[neighbor]:  # 如果新距离小于已知最短距离，则更新最短距离
+                dist[neighbor] = new_dist
+                heapq.heappush(pq, (new_dist, neighbor))  # 将邻居节点加入优先队列
+    return dist
+
+
+
+N, M = map(int, input().split())
+G = [[] for _ in range(N + 1)]  # 图的邻接表表示
+for _ in range(M):
+    s, e, w = map(int, input().split())
+    G[s].append((e, w))
+
+
+start_node = 1  # 源点
+shortest_distances = dijkstra(N, G, start_node)  # 计算源点到各个节点的最短距离
+print(shortest_distances[-1])  # 输出结果
+
